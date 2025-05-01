b9c0733a661635db19ea7ad3864db0062820766b
creamymood <wbwbwlgid@naver.com>
2025-05-01 23:43:53 +0900

    4단계까지 완료

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import { useState } from 'react'
+
 import './App.css'
 import MovieCard from './components/MovieCard.jsx'
-import movieListData from './data/movieListData.json'
 import NavBar from './components/NavBar.jsx'
+import { Routes, Route } from 'react-router-dom';
+import MovieDetail from './components/MovieDetail.jsx'
+import Home from './components/Home.jsx'
+
 
 
 function App() {
-  const [movies,setMovies] = useState(movieListData.results)
+  
 
   return (
     <>
-    <Routes>
         <header>
           <NavBar/>
         </header>
-        <main>
-        
-              <div className="moviecard">
-              {movies.map((movie,id)=>(
-                <MovieCard
-                key={id}
-                title = {movie.title}
-                poster = {movie.poster_path}
-                rating = {movie.vote_average}
-                />
-              ))}
-            </div>
-          
-        </main>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/details/:id' element={<MovieDetail /> }></Route>
+        </Routes>
+      
+      
    </>
   )
 }
--- src/components/MovieDetail.jsx
+++ src/components/MovieDetail.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import MovieDetailData from '../data/movieDetailData.json'
+
 
 function MovieDetail() {
+    const {poster_path, title, vote_average, genres, overview} = MovieDetailData
+
 
     return(
         <div>
+            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="movie poster" />
+            <h1>{title}</h1>
+            <h3>{vote_average}</h3>
+            {genres.map((genre)=>{
+                return <li key={genre.id}>{genre.name}</li>
             
+            })}
+            <h3>{overview}</h3>
         </div>
     )
 }
