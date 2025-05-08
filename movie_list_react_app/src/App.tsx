fc143d163659412e0c264f33a5f0b384dde1754b
Lee-sung-il <wj132612@naver.com>
2025-05-08 13:00:57 +0900

    서버와 연동 테스트 삭제

--- movie_list_react_app/src/App.tsx
+++ movie_list_react_app/src/App.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import { useState} from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import MovieList from './components/MovieList';
 import MovieDetail from './components/MovieDetail';
@@ -18,19 +18,10 @@ function App() {
             return newValue;
         });
     };
-    const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        fetch('/api/message')
-            .then((res) => res.json())
-            .then((data) => setMessage(data.message));
-    }, []);
 
     return (
         <div>
-            <h1>프론트 - 백엔드 연결 테스트</h1>
-            <p>{message}</p>
-
             <Router>
                 <Routes>
                     <Route path="/" element={<Layout isDarkMode={isDarkMode} toggleDarkMode={toggleDarkMode}/>}>
