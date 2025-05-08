7f3355be2f512ab52beae7c6ff8574a655ec6424
허성진 <heoseongjin@heoseongjin-ui-MacBookPro-2.local>
2025-05-08 13:30:12 +0900

    2단계 완료

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
+import { useNavigate } from "react-router-dom"
 import styles from "./Header.module.css"
 function Header () {
+ const Nav = useNavigate()
     return(
         <>
     
         <div className={styles.box}>
-            <h3 className={styles.logo}>SYMovie</h3>
+            <h3 className={styles.logo} onClick={()=>{Nav('/')}}>SYMovie</h3>
             <input type="text"  className={styles.serche} placeholder="검색어를 입력하세요"/>
             <button className={styles.Sbutton}>🔍</button>
             <div className={styles.login}>
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,16 +1,41 @@
 
 import { useNavigate } from "react-router-dom"
 import styles from "./MovieCard.module.css"
+import { useEffect, useState } from "react"
 function MovieCard ({data}) {
     const imgURL = "https://image.tmdb.org/t/p/w400"
     const Nav = useNavigate() 
+    const [movies, setMovies] = useState([])
+    const [loading, setLoading] = useState(false)
+    useEffect(()=>{
+        const fetchMovies = async ()=>{
+            setLoading(true)
+
+            try{
+                const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR', {
+                    headers: {
+                      accept: 'application/json',
+                      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`, 
+                    },
+                  });
+                  const data = await res.json()
+                  const filterMovies = data.results.filter(Movies=>Movies.adult === false)
+                  setMovies(filterMovies)
+            } catch(error){
+                console.error('데이터를 가져오는중 에러 발생함', error)
+             } finally{setLoading(false)}
+            
+        }
+        fetchMovies()
+    },[])
+    if (loading) return <div>로딩 중...</div>;
     return(
 <>
 <div className={styles.CardListBox}>
-    {data.results.map((a,i)=>{
+    {movies.map((a,i)=>{
 
         return(
-        <div className={styles.Card} key={i} onClick={()=>{Nav('/detail')}}>
+        <div className={styles.Card} key={i} onClick={() => Nav(`/detail/${a.id}`)}>
             <img src={`${imgURL}${a.poster_path}`}className={styles.CardImg}/>
             <div className={styles.CardSubData}>
                 <h3 className={styles.CardName}>{a.title}</h3> 
