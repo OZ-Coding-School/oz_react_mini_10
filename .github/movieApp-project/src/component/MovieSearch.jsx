4b7217b106a96358ac1915f272b0ff5367828186
tkdgheee <sangho135790@gmail.com>
2025-05-09 14:56:15 +0900

    오타 수정

--- .github/movieApp-project/src/component/MovieSearch.jsx
+++ .github/movieApp-project/src/component/MovieSearch.jsx
@@ -1,14 +1,16 @@
 import { useSearchParams } from "react-router-dom"
-import { useDebounce } from "../../hook/debounce";
+import { useDebounce } from "../hook/debounce";
 import { useEffect, useState } from "react";
 import { movieSearchAPI } from "../movieAPI/movieSearchAPI";
+import MovieCard from "./moviecard";
 
-const movieSearch = () => {
-    const [searchParams] = useSearchParams;
+const MovieSearch = () => {
+    const [searchParams] = useSearchParams();
+    // useSearchParams로 querystring 형태로 url 값을 가져올 거임.
     const query = searchParams.get('query') || '';
+    // search 
     const debouncedQuery = useDebounce(query, 500)
     const [results, setResults] = useState([])
-
     useEffect(() => {
         const fetchResults = async () => {
             const data = await movieSearchAPI(debouncedQuery)
@@ -16,4 +18,25 @@ const movieSearch = () => {
         }
         if (debouncedQuery) fetchResults();
     },[debouncedQuery])
+
+    return (
+        <div className="flex justify-center items-center flex-wrap gap-8 w-full h-full">
+         {results.length > 0 ? (
+          results.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              id={movie.id}
+              title={movie.title}
+              posterPath={movie.poster_path}
+              voteAverage={movie.vote_average}
+            />
+          ))
+        ) : (
+          <p>검색 결과가 없습니다.</p>
+        )}
+      </div>
+    )
 }
+
+
+export default MovieSearch
