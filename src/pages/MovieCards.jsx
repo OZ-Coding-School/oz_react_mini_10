3eedf26d1d42ddbdb36814619ab8a7c0e274ae41
Js00223 <jaemunheo8@gmail.com>
2025-05-09 13:39:11 +0900

    mission2 complete

--- src/pages/MovieCards.jsx
+++ src/pages/MovieCards.jsx
@@ -1,25 +1,41 @@
+import { useEffect, useState } from "react";
 import MovieCard from "../components/MovieCard";
-import movieList from "../data/movieListData.json";
-import MovieDetail from "../data/movieDetailData.json";
 import { Link } from 'react-router-dom';
 
 const MovieCards = () => {
+  const [movies, setMovies] = useState([]);
   const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
 
+  useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', {
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+      },
+    })
+      .then(res => res.json())
+      .then(data => {
+        const filtered = data.results.filter(movie => !movie.adult);
+        setMovies(filtered);
+      })
+      .catch(err => console.error("영화 데이터를 불러오지 못했습니다:", err));
+  }, []);
+
   return (
-    
     <div
       className="movie-list"
       style={{ display: "flex", flexWrap: "wrap", gap: "1rem" }}
-    > 
-    {movieList.results.map((movie, i) => (
-        <Link to={`/details`} style={{ textDecoration: "none", color: "inherit" }}>
-    <MovieCard key ={i} movieList={movie}/>
-  </Link>
-        
-))}
+    >
+      {movies.map((movie, i) => (
+        <Link
+          key={movie.id}
+          to={`/details/${movie.id}`}
+          style={{ textDecoration: "none", color: "inherit" }}
+        >
+          <MovieCard movieList={movie} />
+        </Link>
+      ))}
     </div>
-    
   );
 };
 
